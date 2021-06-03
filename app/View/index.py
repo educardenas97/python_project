280e039640a82760461f1017f879bbc864f1acb9
Eduardo Cardenas <eduardocardenas97@gmail.com>
2021-06-02 21:39:35 -0400

    Interfaz asquerosa en menu.pu

---
+++ app/View/index.py
@@ -0,0 +1,7 @@
+from tkinter import *
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack
+        self.crear_widget(self)
+
--- app/View/menu.py
+++ app/View/menu.py
@@ -1,22 +1,72 @@
-from app.View.administracion import main as administracion
-from app.View.paqueteria import main as paqueteria
-from app.View.transportes import main as transportes
-from app.View.funciones import *
-
-def menu():
-    #Opciones a mostrar en el menu
-    items = ["Administracion", "Paquetería", "Transporte", "Salir"]
-    secciones = [administracion, paqueteria, transportes]
-    #Se ejecuta una subrutina para permitir el input 
-    options = sub_menu(items, "Menu Principal")
-
-    while options != len(items): #Loop para seleccionar items
-        if options != len(items):
-            secciones[options-1]()
-        #Se vuelve a habilitar un input para el usuario
-        options = sub_menu(items, "Menu Principal")
-    
-    return True
+from tkinter import *
+from app.Core.main import *
 
 
-main(menu)
+root = Tk()
+
+#Menu Bar
+def main():
+    menu_bar =  Menu(root)
+    root.geometry("400x500")
+    root.config(menu=menu_bar)
+
+    administracion_bar = Menu(menu_bar, tearoff=0)
+    administracion_bar.add_command(
+        label="Registrar Cliente", command=registrar_cliente)
+    administracion_bar.add_command(
+        label="Registrar Empleado", command=registrar_empleado)
+
+    paqueteria_bar = Menu(menu_bar, tearoff=0)
+    paqueteria_bar.add_command(label="Registrar Paquete")
+    paqueteria_bar.add_command(label="Ver paquetes en transito")
+    paqueteria_bar.add_command(label="Ver paquetes registrados")
+
+    transporte_bar = Menu(menu_bar, tearoff=0)
+    transporte_bar.add_command(label="Registrar transporte")
+    transporte_bar.add_command(label="Ver transportes registrados")
+
+    menu_bar.add_cascade(label="Administracion", menu=administracion_bar)
+    menu_bar.add_cascade(label="Paqueteria", menu=paqueteria_bar)
+    menu_bar.add_cascade(label="Transporte", menu=transporte_bar)
+
+def registrar_cliente():
+    root.title("Registrar Cliente")
+
+    text = Label(root, text="Nombre: ")
+    text.grid(row=0, column=0, padx=2)
+
+    nombre = Entry(root, width=30)
+    nombre.grid(row=0, column=1, padx=2)
+
+    text = Label(root, text="Apellido: ")
+    text.grid(row=1, column=0, padx=2)
+
+    apellido = Entry(root, width=30)
+    apellido.grid(row=1, column=1, padx=2)
+
+    text = Label(root, text="Cedula: ")
+    text.grid(row=2, column=0, padx=2)
+
+    cedula = Entry(root, width=30)
+    cedula.grid(row=2, column=1, padx=2)
+
+
+def registrar_empleado():
+    root.title("Registrar Empleado")
+
+    text = Label(root, text="Nombre: ")
+    text.grid(row=0, column=0)
+
+    nombre = Entry(root, width=30)
+    nombre.grid(row=0, column=1)
+
+    text = Label(root, text="Apellido: ")
+    text.grid(row=1, column=0)
+
+    apellido = Entry(root, width=30)
+    apellido.grid(row=1, column=1)
+
+
+
+main()
+root.mainloop()
